ceba5724b5349ad1ca92421302ebe90f637a33f9
Krearden <krearden@yandex.ru>
2022-10-06 19:05:23 +0500

    getLUfromA не работает getSummOfMultiply_ij работает (мб) ༼ つ ◕_◕ ༽つ

--- main.py
+++ main.py
@@ -26,16 +26,42 @@ def printMatrix(matrix):
     if matrix:
         for row in matrix:
             for element in row:
-                print("{} ".format(element), end="")
+                print("{:10.5f} ".format(element), end="")
             print()
     else:
         print("Empty matrix given")
 
+#get LU matrix from A matrix
+def getLUfromA(A_matrix):
+    n = len(A_matrix)
+    for j in range(n):
+        for i in range(n):
+            number = min(i, j) - 1
+            summ_of_multiply = getSummOfMultiply_ij(A_matrix, i, j, number)
+            A_matrix[i][j]  = A_matrix[i][j] - summ_of_multiply
+            if (i > j):
+                A_matrix[i][j] = A_matrix[i][j] / A_matrix[j][j]
+    return A_matrix
+
+
+
+# сумма произведений элементов i-той j-того столбца матрицы
+def getSummOfMultiply_ij(matrix, i, j, n):
+    summ = 0.0
+    for k in range(n):
+        summ += matrix[i][k] * matrix[k][j]
+        print("{} * {} + \n".format(matrix[i][k], matrix[k][j]))
+    return summ
+
 
 #MAIN
 filepath = "files/"
-filename = "var_18_a.txt"
-matrix_18_a = readMatrixFromFile(filepath + filename)
-printMatrix(matrix_18_a)
+filename = "var_18_b.txt"
+A_matrix = readMatrixFromFile(filepath + filename)
+LU_matrix = getLUfromA(A_matrix)
+print("A:")
+printMatrix(A_matrix)
+print("LU:")
+printMatrix(LU_matrix)
 
 
