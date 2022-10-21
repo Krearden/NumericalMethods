66ab40086a32d3382984e84c574d7e50f9cfd3ad
Krearden <->
2022-10-21 13:03:36 +0500

    fix L later

--- main.py
+++ main.py
@@ -35,32 +35,49 @@ def printMatrix(matrix):
 def LU(A):
     n = len(A)
     P = list([[1 if i == j else 0 for j in range(n)] for i in range(n)]) #матрица перестановок
-
+    vector_p = [0, 1, 2, 3]
+    L = list([[0 for j in range(n)] for i in range(n)])
+    U = A.copy()
     for i in range(n):
-
         #находим главный элемент в столбце i и меняем строки местами
         max_el = 0
         max_el_row_index = -1
         for k in range(i, n):
-            if(abs(A[k][i]) > max_el):
-                max_el = abs(A[k][i])
+            if(abs(U[k][i]) > max_el):
+                max_el = abs(U[k][i])
                 max_el_row_index = k
         if (max_el != 0 and max_el_row_index != i):
             P = swapRows(P, i, max_el_row_index)
-            A = swapRows(A, i, max_el_row_index)
-
-        main_element = A[i][i]
+            A = swapRows(U, i, max_el_row_index)
+            vector_p = swapRows(vector_p, i, max_el_row_index)
+        #метод гаусса с перестановкой строк (формируем матрицу U)
+        main_element = U[i][i]
+        L[i][i] = main_element
         for row_element in range(n):
-            A[i][row_element] /= main_element
+            U[i][row_element] /= main_element
         for k in range(i + 1, n):
-            element_to_multiply = A[k][i] #- элемент на который умножать (первый элемент каждой строки ниже i-той)
+            element_to_multiply = U[k][i] #- элемент на который умножать (первый элемент каждой строки ниже i-той)
             for j in range(i, n):
-                A[k][j] = A[k][j] - A[i][j] * element_to_multiply
+                U[k][j] = U[k][j] - U[i][j] * element_to_multiply
+
+        #формируем матрицу L ПОКА НЕ РАБОТАЕТ потому что А меняется вместе с У (why?)
+        for j in range(i):
+            summ = 0
+            for k in range(j):
+                summ += L[i][k] * U[k][j]
+            L[i][j] = A[vector_p[i]][j] - summ
+
+
+
         print("i = {}".format(i))
-        printMatrix(A)
+        printMatrix(U)
         print()
-    print("U matirx made from A: ")
+    print("A: ")
     printMatrix(A)
+    print("U: ")
+    printMatrix(U)
+    print("L: ")
+    printMatrix(L)
     print("P: ")
     printMatrix(P)
     return A
