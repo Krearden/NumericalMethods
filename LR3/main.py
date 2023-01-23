64e01ba5108d47848d8602bbbb6ef5e1972a7f6a
Krearden <->
2023-01-23 09:28:59 +0500

    не рабатет backup

--- LR3/main.py
+++ LR3/main.py
@@ -42,25 +42,20 @@ def getEucledianNorm(A):
                     max_non_diag = abs(A[i][j])
                     I = i
                     J = j
-
         theta = math.atan(2 * A[I][J] / (A[I][I] - A[J][J])) / 2
         c = math.cos(theta)
         s = math.sin(theta)
-
         for m in range(n):
             if (m != I and m != J):
                 A_copy[m][I] = c * A[I][m] + s * A[J][m]
                 A_copy[m][J] = -s * A[I][m] + c * A[J][m]
                 A_copy[I][m] = c * A[I][m] + s * A[J][m]
                 A_copy[J][m] = -s * A[I][m] + c * A[J][m]
-
         A_copy[I][I] = c * c * A[I][I] + 2 * s * c * A[I][J] + s * s * A[J][J]
         A_copy[J][J] = s * s * A[I][I] - 2 * s * c * A[I][J] + c * c * A[J][J]
         A_copy[I][J] = (c * c - s * s) * A[I][J] + s * c * (A[J][J] - A[I][I])
         A_copy[J][I] = (c * c - s * s) * A[I][J] + s * c * (A[J][J] - A[I][I])
-
         A = [[A_copy[i][j] for j in range(n)] for i in range(n)]
-
     max_diagonal = 0
     for i in range(n):
         if (abs(A[i][i] > max_diagonal)):
@@ -105,31 +100,46 @@ def getDerivativeMatrix(variant, x, y):
     return dF
 
 
-def timetostop(X, variant):
+def length(X, variant):
     Fx = getFx(X, variant)
     sqrt = math.sqrt(Fx[0] * Fx[0] + Fx[1] * Fx[1])
     return sqrt
 
 
+def getDeterminant(matrix):
+    if (len(matrix) != 2):
+        print("Wrong matrix size")
+    else:
+        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+
 #Newton's method
 def methodNewton(X, variant):
     epsilon = 1e-04
-    iteration_counter = 0
-    N = len(X)
-    newX = [0 for i in range(N)]
-    while (timetostop(X, variant) > epsilon):
-        # #find derivative matrix ВООБЩЕ ЧЕ ТО НЕ ТО СЧИТАЕТ ЕМАЕ
-        dF = getDerivativeMatrix(0, X[0], X[1])
-        # dF_inverse = getInverseMatrix(dF)
-        # newX = vectorSubstraction(X, multipMatrixByVector(dF_inverse, getFx(X, variant)))
-        # X = [newX[i] for i in range(len(X))]
-        # iteration_counter += 1
-        # print(newX)
-        d = dF[0][0] * dF[1][1] - dF[1][0] * dF[0][1]
-        d_x = getFx(X, aaaaa!)
-        d_y =
-
-
+    while (length(X, variant) > epsilon):
+        #матрица производных
+        dF = getDerivativeMatrix(variant, X[0], X[1])
+        #определители
+        determinant = getDeterminant(dF)
+        #fx
+        Fx = getFx(X, variant)
+        #for x
+        dF_x = [[dF[i] for i in range(len(dF))] for i in range(len(dF))]
+        dF_x[0][0] = Fx[0]
+        dF_x[1][0] = Fx[1]
+        determinant_x = getDeterminant(dF_x)
+        #for y
+        dF_y = [[dF[i] for i in range(len(dF))] for i in range(len(dF))]
+        dF_y[0][1] = Fx[0]
+        dF_y[1][1] = Fx[1]
+        determinant_y = getDeterminant(dF_y)
+        #find new x and y
+        newX = [0 for i in range(len(dF))]
+        newX[0] = X[0] - determinant_x / determinant #find new X
+        newX[1] = X[1] - determinant_y / determinant #find new Y
+        #copy
+        X = [newX[i] for i in range(len(dF))]
+    print(X)
+    return X
 
 #MAIN
 X = [0, 0.8] # [0][0] is X and [0][1] is Y (примерные значения)
