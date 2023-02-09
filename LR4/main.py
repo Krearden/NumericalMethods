178e82709e18f52e6474d4d97c3ac0633def538e
krearden <krearden@yandex.ru>
2023-02-09 19:16:00 +0500

    newton interp dont work commit for backup

--- LR4/main.py
+++ LR4/main.py
@@ -16,29 +16,62 @@ def getFx(x, variant):
     else:
         print("Not done yet")
 
-# def createSplitDiffTable(variant):
-#     a = 1
-#     b = 2
-#     n = 5
-#     h = (b - a) / n
-#     xs = [0 for i in range(n)]
-#     differences_table = [[] for i in range(n)]
-#     while(a <= b):
-#         xs.append(a)
-#         differences_table[0].append(getFx(a, variant))
-#         a += h
-#     for i in range(1, n):
-#         for j in range(n - i):
-#             differences_table[i].append((differences_table[i - 1][j + 1] - differences_table[i - 1][j]) / (xs[j + i] - xs[j]))
-#
-#     return xs, differences_table
-#
+def getH(a, b, n):
+    return (b - a) / (n - 1)
+
+def createSplitDiffTable(variant, a, b, n):
+    h = getH(a, b, n)
+    xs = []
+    differences_table = [[] for i in range(n)]
+    while(a <= b):
+        xs.append(a)
+        differences_table[0].append(getFx(a, variant))
+        a += h
+        a = round(a, 2)
+
+    for i in range(1, n):
+        for j in range(n - i):
+            differences_table[i].append((differences_table[i - 1][j + 1] - differences_table[i - 1][j]) / (xs[j + i] - xs[j]))
+
+    return xs, differences_table
+
+def printSplitDiffTable(xs, differences_table):
+    print("Таблица разделенных разностей:")
+    for i in range(len(xs)):
+        print("{:2.1f}".format(xs[i]), end = '  ')
+        for j in range(len(differences_table[i])):
+            print(differences_table[j][i], end = ' ')
+        print()
+
+def getNewtonPolynom(x, xs, differences_table):
+    n = len(xs)
+    y = 0
+    part_summ = 0
+    for i in range(n):
+        y += part_summ * differences_table[i][0]
+        part_summ *= x - xs[i]
+    return y
+
+def printNewtonInterpolation(a, b, n, xs, differences_table):
+    h = getH(a, b, n)
+    a += h / 2
+    while (a < b):
+        Fx = getFx(a, variant)
+        newtonFx = getNewtonPolynom(a, xs, differences_table)
+        print("{:2.3f}, {}, {}".format(a, Fx, newtonFx))
+        a += h / 2
 
 
 #MAIN
 if __name__ == '__main__':
     variants = [0]
     for variant in variants:
-        xs, difft = createSplitDiffTable(variant)
-        for i in range(len(xs)):
-            print(xs[i], difft[i])
+        a = 1
+        b = 2
+        n = 6
+        xs, difft = createSplitDiffTable(variant, a, b, n)
+        printSplitDiffTable(xs, difft)
+        print()
+        printNewtonInterpolation(a, b, n, xs, difft)
+        # for i in range(len(xs)):
+        #     print(xs[i], difft[i])
