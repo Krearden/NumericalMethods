df083028c8dc532a3f0d8fbea72dcc7bb0526e6b
krearden <krearden@yandex.ru>
2023-03-19 18:44:24 +0500

    LR5

--- LR5/main.py
+++ LR5/main.py
@@ -9,13 +9,13 @@ import sys
 
 
 #GLOBAL VARIABLES
-variant = 18
+variant = 666
 count_uses = 0
 count_uses_dx = 0
 
 
 # delete comment below if need to write to file
-# file_path = f'lr5(6)_ped_zap_v{variant}.txt'
+# file_path = f'lr6(5)_ped_zap_v{variant}.txt'
 # sys.stdout = open(file_path, "w")
 
 
@@ -29,6 +29,8 @@ def f(x):
         return pow(3, x) - 2 * x + 5
     elif (variant == 27):
         return pow(e, -2 * x) - 2 * x + 1
+    elif (variant == 666):
+        return pow(2, x) - 3 * x + 2
 
 #производная от фукнции
 def dfdx(x):
@@ -38,6 +40,8 @@ def dfdx(x):
         return log(3) * pow(3, x) - 2
     elif (variant == 27):
         return -2 * pow(e, -2 * x) - 2
+    elif (variant == 666):
+        return pow(2, x) * log(2) - 3
 
 #первообразная от фукнции
 def F(x):
@@ -45,6 +49,8 @@ def F(x):
         return pow(3, x) / log(3) - (x * x) + 5 * x
     elif (variant == 27):
         return -(x * x) + x - pow(e, -2 * x) / 2
+    elif (variant == 666):
+        return pow(2, x) / log(2) - 1.5 * pow(x, 2) + 2 * x
 
 #формула Ньютона-Лейбница
 def NewtonLeybnic(a, b):
@@ -128,7 +134,7 @@ def gaussMethod(a, b, h):
 
 
 #Решение определенного интеграла с выбором метода, погрешность по Рунге
-def solveIntegral(a, b, integral_sum_method, diff, alpha, epsilon):
+def solveIntegral(a, b, integral_sum_method, approx_order, alpha, epsilon):
     global count_uses
     global count_uses_dx
     n = 1
@@ -137,7 +143,7 @@ def solveIntegral(a, b, integral_sum_method, diff, alpha, epsilon):
     I_before = 0
     I_2before = 0
     l = 1 / log(alpha)
-    delta_r_coef = pow(alpha, diff) / (1.0 - pow(alpha, diff))
+    delta_r_coef = pow(alpha, approx_order) / (1.0 - pow(alpha, approx_order))
     print("|  N  |    h    | Интеграл     | Погрешность (отн.)  | Оценка погр. по Рунге     |  k     |")
     while (True):
         #обнуляем счетчики обращений к фукнции и производной
@@ -146,7 +152,7 @@ def solveIntegral(a, b, integral_sum_method, diff, alpha, epsilon):
         count_uses_dx = 0
         #вычисляем интеграл выбранным численным методом
         I = integral_sum_method(a, b, h)
-        #Вычисляем оценку порядка сходимости. Должно приближаться к diff - порядку сходимости.
+        #Вычисляем оценку порядка сходимости. Должно приближаться к approx_order - порядку сходимости.
         if (n != 1 and n != 2):
             k = l * log((I - I_2before) / (I_before - I_2before) - 1.0)
         else:
@@ -176,7 +182,7 @@ def solveIntegral(a, b, integral_sum_method, diff, alpha, epsilon):
         n = ceil((b - a) / h)
 
     print(f"Результат = {I:.12f}")
-    print(f"Количество обращений к фукнции = {count_uses}", end = "")
+    print(f"Количество обращений к функции = {count_uses}", end = "")
     if (count_uses_dx != 0):
         print(f" + {count_uses_dx}", end = "")
 
@@ -189,6 +195,7 @@ if __name__ == '__main__':
     alpha = 0.5
     print(f"Вариант {variant}")
     print(f"Точное значение интеграла J = {NewtonLeybnic(a, b):.16f}")
+    print(f"Заданная точность вычислений = {epsilon}")
     print("\n")
     print("Метод трапеций")
     solveIntegral(a, b, trapezoidalMethod, 2, alpha, epsilon)
